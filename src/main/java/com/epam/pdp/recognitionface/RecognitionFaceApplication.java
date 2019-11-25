0e70eccfa86e667d9ca67638b5d9a6b5317528b8
p0keT <p0keT@ukr.net>
2019-11-25 22:39:51 +0200

    Initial commit

---
+++ src/main/java/com/epam/pdp/recognitionface/RecognitionFaceApplication.java
@@ -0,0 +1,13 @@
+package com.epam.pdp.recognitionface;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecognitionFaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecognitionFaceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/epam/pdp/recognitionface/RecognitionFaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.pdp.recognitionface;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecognitionFaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
